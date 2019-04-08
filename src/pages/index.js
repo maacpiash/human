3061e0c35ab6041fd18455ecdc73de4a42d81962
Md. Abdul Ahad Chowdhury <maacpiash@outlook.com>
2019-04-09 02:14:30 +0600

    Alhamdu lillah done with first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Landing from '../sections/Landing';
+import About from '../sections/About';
+import Projects from '../sections/Projects';
+import Writing from '../sections/Writing';
+import Header from '../components/Header';
+
+const IndexPage = () => (
+  <Layout>
+    <Header />
+    <Landing />
+    <About />
+    <Projects />
+    <Writing />
+  </Layout>
+);
+
+export default IndexPage;
